cbac0f86cd14848d7d907023778230e454bb67c2
Jp3222 <juanpc.559@gmail.com>
2026-07-24 07:52:21 -0600

    first commit

---
+++ src/main/java/com/jsoftware/jbluemodel/JBlueModel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jsoftware.jbluemodel;
+
+/**
+ *
+ * @author juanp
+ */
+public class JBlueModel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
